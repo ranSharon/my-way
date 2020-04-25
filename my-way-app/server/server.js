9c41d84077ab5f0b5d6b3e165e4855e7095298fd
ranSharon <ransh360@gmail.com>
2020-04-25 18:15:18 +0300

    Connected to database

--- my-way-app/server/server.js
+++ my-way-app/server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+
+// database
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
